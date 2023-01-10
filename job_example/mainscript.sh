827d02f3da8df84e4bb047015b0dd4e51a06f968
Clark Hale <xlark@sdf.org>
2023-01-10 17:22:30 +0000

    fixed quoting issue

--- job_example/mainscript.sh
+++ job_example/mainscript.sh
@@ -4,10 +4,10 @@ echo "This is a test script to ensure that everything is working"
 
 echo "My environment:"
 
-env | sed -i 's/^/   /"
+env | sed -i 's/^/   /'
 
 echo "Directory of /scripts"
 
-find /scripts | sed -i 's/^/   /"
+find /scripts | sed -i 's/^/   /'
 
 
